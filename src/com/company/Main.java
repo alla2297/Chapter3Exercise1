e8708503b8f5727bfea49cc1555986660051390d
alla2297 <alla2297@edu.easj.dk>
2021-09-07 12:37:58 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.printf("Four thirds = %.3f", 4.0 / 3.0);
+        System.out.println("\nnext line show a error:");
+        System.out.printf("Four thirds = %,d",4.0 / 3.0);
+
+    }
+}
